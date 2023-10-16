aecf8e42b74ef9816a55c8efe0e84a8ad04da3d5
Kimutai-cloud <kevin.kimu0306@gmail.com>
2023-10-16 23:07:27 +0300

    commit

--- Todo1/main.py
+++ Todo1/main.py
@@ -1,20 +1,19 @@
 import cairo
 
-surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 1000, 1000)
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 500, 500)
 ctx = cairo.Context(surface)
 ctx.set_source_rgb(0.8, 0.8, 0.8)
 ctx.paint()
 
-ctx.rectangle(150, 100, 100, 240)
-ctx.set_source_rgb(1, 0, 0)
-ctx.fill()
-
-ctx.rectangle(250, 200, 100, 240)
+ctx.move_to(50, 100)
+ctx.line_to(200, 50)
+ctx.line_to(250, 300)
+ctx.line_to(100, 200)
+ctx.close_path()
 ctx.set_source_rgb(0, 1, 0)
+ctx.set_line_width(50)
 ctx.stroke()
 
-ctx.rectangle(350, 300, 100, 240)
-ctx.set_source_rgb(1, 0, 0)
-ctx.fill()
+
 
 surface.write_to_png("example1.png")
